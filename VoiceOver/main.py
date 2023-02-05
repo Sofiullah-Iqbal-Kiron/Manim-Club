babbc7451bea4ede6ed3b222dfdb52b2b0623b1b
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2023-02-05 14:04:25 +0600

    Added voiceover at fourier series difinition video and published on my basic/first youtube channel.

--- VoiceOver/main.py
+++ VoiceOver/main.py
@@ -7,6 +7,36 @@ from manim_voiceover.services.gtts import GTTSService
 # Azure Voice Service requires clear internet connnection and
 from manim_voiceover.services.azure import AzureService
 
+# Own recording service
+from manim_voiceover.services.recorder import RecorderService
+
+config.background_color = "#e0e6e2"  # None #
+Tex.set_default(color=BLACK)
+Text.set_default(color=BLACK)
+Mobject.set_default(color=RED)
+Dot.set_default(color=BLACK)
+VMobject.set_default(color=BLACK, stroke_width=4)
+Square.set_default(color=GREEN)
+
+main_tex = config.tex_template
+
+r_preamble = r"""
+\usepackage[document]{ragged2e}
+\usepackage[usestackEOL]{stackengine}
+\usepackage
+{
+	polyglossia,
+	anyfontsize,
+	tcolorbox
+}
+
+\setmainlanguage[numerals=Devanagari]{bengali}
+\setotherlanguage{english}
+\newfontfamily\bengalifont[Script=Bengali]{Kalpurush}
+"""
+
+bn_tex = TexTemplate(preamble=r_preamble, tex_compiler="lualatex")
+
 
 class Main(VoiceoverScene):
     def construct(self):
@@ -28,3 +58,21 @@ class TextAzure(VoiceoverScene):
         with self.voiceover(
                 text="This is exampling with AzureVoice service. Hello Kiron. Let's talk some dirty you naughty boy.") as tra:
             self.play(banner.create(), run_time=tra.duration)
+
+
+class MathTexVoiceOver(VoiceoverScene):
+    def construct(self):
+        self.set_speech_service(AzureService(voice="bn-BD-PradeepNeural", style="newcast-casual"))
+        tex = Tex(r"$a^2+2ab+b^2$").scale(2)
+        with self.voiceover(
+                text="আমার কাছে একটি বীজগাণিতিক রাশি রয়েছে। এ এর বর্গ যোগ এ এবং বি এর গুণফলের দ্বিগুণের সাথে বি এর বর্গের সমষ্টি।") as tracker:
+            self.play(Write(tex), run_time=tracker.duration)
+
+
+class CallHasib(VoiceoverScene):
+    def construct(self):
+        self.set_speech_service(AzureService(voice="bn-BD-NabanitaNeural", style="newcast-casual"))
+        text = Tex(r"\textbf{হাসিব ভাই} $০৩৭$", tex_template=bn_tex).scale(
+            2)  # bug: follow1: https://github.com/ManimCommunity/manim-voiceover/blob/main/examples/quadratic-formula-arabic.py, follow2: https://github.com/3b1b/manim/issues/525
+        with self.voiceover(text="হাসিব ভাই ০৩৭") as tracker:
+            self.play(Write(text), run_time=tracker.duration)
--- What is Fourier Series/fourier_series.py
+++ What is Fourier Series/fourier_series.py
@@ -1,13 +1,23 @@
 from manim import *
 
+# VoiceOver imports
+from manim_voiceover import VoiceoverScene
+from manim_voiceover.services.gtts import GTTSService
+from manim_voiceover.services.azure import AzureService
 
-class FourierSeries(Scene):
+
+class FourierSeries(VoiceoverScene):
     def construct(self):
+        myTemplate = TexTemplate()
+        myTemplate.add_to_preamble(r"\usepackage{csquotes}")
         # self.add(NumberPlane())
         self.always_update_mobjects = True
-        subscribe = Text("Subscribe", font_size=15, color=WHITE).move_to([6.5, 4.7, 0])
+        self.set_speech_service(AzureService(voice="en-GB-OliverNeural", style="newscast-casual"))
+        # self.set_speech_service(GTTSService())
+
+        # subscribe = Text("Subscribe", font_size=15, color=WHITE).move_to([6.5, 4.7, 0])
         # subscribe.shift(6.5 * RIGHT)
-        self.add(subscribe)
+        # self.add(subscribe)
 
         ourBasicTrigonometricSeriesIs = Text(text="We know that the basic trigonometric series is: ", font="Arial",
                                              font_size=52, weight=SEMIBOLD, width=12, color=GREEN)
@@ -15,26 +25,40 @@ class FourierSeries(Scene):
                                                 font_size=48)
         g1 = Group(ourBasicTrigonometricSeriesIs, theBasicTrigonometricEquation).arrange(DOWN, buff=MED_LARGE_BUFF)
 
-        self.play(AddTextLetterByLetter(ourBasicTrigonometricSeriesIs), run_time=2)
-        self.play(FadeIn(theBasicTrigonometricEquation), run_time=3)
+        with self.voiceover(text="We know that the basic trigonometric series is...") as tracker:
+            self.play(AddTextLetterByLetter(ourBasicTrigonometricSeriesIs), run_time=tracker.duration / 2)
+
+        with self.voiceover(text="the equation appearing on the screen.") as tracker:
+            self.play(FadeIn(theBasicTrigonometricEquation), run_time=tracker.duration)
+
         self.play(g1.animate.scale(0.65).shift(2.9 * UP))
         self.wait(1)
 
-        itWillCalled = Tex(r"It will be called \textbf{Fourier Series} if the terms $A_0, A_n, B_n$ is:",
-                           font_size=40).shift(1.5 * LEFT)
+        itWillCalled = Tex(
+            r'This equation will be called \textbf{\enquote{Fourier Series}} if the terms $A_0, A_n, B_n$ could be substituted into:',
+            tex_template=myTemplate, font_size=34)
         A0 = MathTex(r"A_0 = \frac{1}{2\pi}\int_{-\pi}^{\pi} f(x)\cdot dx", font_size=40)
         An = MathTex(r"A_n = \frac{1}{\pi}\int_{-\pi}^{\pi} f(x)\cdot \cos nx\cdot dx", font_size=40)
         Bn = MathTex(r"B_n = \frac{1}{\pi}\int_{-\pi}^{\pi} f(x)\cdot \sin nx\cdot dx", font_size=40)
         g2 = Group(A0, An, Bn).arrange(DOWN, buff=MED_SMALL_BUFF).move_to(0.5 * DOWN)
         whereFX = Tex(r"Where \textbf{$f(x)$} is any single-valued function defined in interval $(-\pi, \pi)$.",
                       font_size=35).next_to(g2, 1 * DOWN)
+        with self.voiceover(
+                text="This equation will be called \"Fourier Series\" if the terms, A nought, A N, B N could be substituted into") as tracker:
+            self.play(Create(itWillCalled), run_time=tracker.duration / 1.6)
 
-        self.play(Create(itWillCalled), run_time=2)
-        self.wait(1)
         self.play(itWillCalled.animate.scale(0.7).next_to(g2, 1.3 * UP))
         self.play(itWillCalled.animate.set_fill(RED), SpinInFromNothing(g2), point_color=RED)
-        self.play(Indicate(g2))
-        self.play(Write(whereFX), run_time=3)
-        self.play(whereFX.animate(run_time=1.2).shift(0.5 * DOWN).scale(1.3))
-        self.play(Circumscribe(whereFX))
+
+        with self.voiceover(text="these equations.") as tracker:
+            self.play(Indicate(g2), run_time=tracker.duration)
+
+        with self.voiceover(
+                text="Where F of x is any single valued function defined in the exclusive interval from minus pai to pai.") as tracker:
+            self.play(Write(whereFX), run_time=tracker.duration / 1.8)
+
+        self.play(whereFX.animate(run_time=1.2).shift(0.5 * DOWN).scale(1.15))
+
+        with self.voiceover(text="Thank you for watching.") as tracker:
+            self.play(Circumscribe(whereFX), run_time=tracker.duration * 1.2)
         self.wait(2)
