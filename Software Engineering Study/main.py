3d1031a7432d9c05a0be7e21103d5ea3cdb401bc
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2023-11-06 01:16:38 +0600

    learning updater method

--- Software Engineering Study/main.py
+++ Software Engineering Study/main.py
@@ -1,30 +1,25 @@
 from manim import *
+from components import Usecase
 
 
 class Main(Scene):
     def construct(self):
-        actors=['todo: svg here']
+        actors = ['todo: svg here']
 
         cases_text = [
-            'Add\na task',
-            'Remove\na task',
+            'Add a task',
+            'Remove a task',
             'Mark a task\nas done',
             'Mark a task\nas important',
             'Remove all tasks\nin one go',
             'Save tasks',
             'Post tasks\nto sever database',
         ]
-        cases_Paragraph = [Paragraph(case, alignment='center') for case in cases_text]
 
-        case_group = Group(*cases_Paragraph).arrange(DOWN, 0.5).scale(0.6).shift(LEFT)
-        system_heading=Text("Your Daily Tasks", weight=BOLD)
-        system_rectangle=Rectangle()
+        cases = [Usecase(case, 0.5) for case in cases_text]
+        cases = Group(*cases).arrange(DOWN, 0.3).scale(0.6).shift(LEFT*2).shift(DOWN*0.5)
 
-        add_a_task=Text('Add a Task.')
-        case_surround_oval = Ellipse(width=4, height=2.5, stroke_width=2, stroke_color=BLUE_A).surround(add_a_task)
-        self.play(Create(case_surround_oval), Write(add_a_task))
-
-        # for c in cases_in_paragraph:
-        #     self.play(Write(c))
-
-        # self.play(g1.animate.arrange(DOWN, buff=1))
+        system_heading=Text("Your Daily Tasks", weight=BOLD, font_size=24).next_to(cases,UP*1.5)
+        system=Group(cases, system_heading)
+        system_rectangle=Rectangle(height=system.height+2, width=system.width+0.5).surround(system)
+        self.add(system, system_rectangle)
--- Updater Method/main.py
+++ Updater Method/main.py
@@ -3,6 +3,15 @@ from manim import *
 
 class Main(Scene):
     def construct(self):
-        dot = Dot(radius=0.5)
-        label = Text(text=dot.get_x()).next_to(dot, UP, buff=0.5)
-        self.add(dot, label)
+        def update_label_text(object):
+            object.set(text=str(cnt))
+            object.next_to(dot, UP)
+            cnt += 1
+
+        cnt = 1
+        dot = Dot(ORIGIN)
+        label = Text(str(dot.get_x())).next_to(dot, UP)
+        label.add_updater(update_label_text)
+        self.add(label)
+        self.play(dot.animate.move_to(UL*3))
+        self.play(dot.animate.move_to(DOWN*3))
